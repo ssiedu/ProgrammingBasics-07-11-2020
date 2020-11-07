b06cd5fd01948a561150e6405f0ffb6118f60b1c
manoj <manojs121@rediffmail.com>
2020-11-07 17:28:02 +0530

    07-11

---
+++ src/Demo.java
@@ -0,0 +1,10 @@
+
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello From Main Of Demo..");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+    }
+}
---
+++ src/Trial.java
@@ -0,0 +1,5 @@
+public class Trial {
+    public void show(){
+        System.out.println("Hello From Show of Trial...");
+    }
+}
